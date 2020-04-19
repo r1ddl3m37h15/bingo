91cf8a00c20b2052450f329ebb1cb03d7b27d00f
jeff suess <jsuess@gmail.com>
2020-04-19 11:49:04 -0400

    main: check for bingo. add logging.

--- bingo.py
+++ bingo.py
@@ -10,8 +10,6 @@
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
@@ -31,9 +29,13 @@ __license__ = 'MIT'
 import random, sys
 from termcolor import colored
 import os
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', filename='bingo.log', level=logging.DEBUG)
+
 
 
 ########## global vars ##########
+
 
 
 # column spacer
@@ -47,17 +49,24 @@ coli = ["I 16","I 17","I 18","I 19","I 20","I 21","I 22","I 23","I 24","I 25","I
 coln = ["N 31","N 32","N 33","N 34","N 35","N 36","N 37","N 38","N 39","N 40","N 41","N 42","N 43","N 44","N 45"]
 colg = ["G 46","G 47","G 48","G 49","G 50","G 51","G 52","G 53","G 54","G 55","G 56","G 57","G 58","G 59","G 60"]
 colo = ["O 61","O 62","O 63","O 64","O 65","O 66","O 67","O 68","O 69","O 70","O 71","O 72","O 73","O 74","O 75"]
+freespace = ["Free"]
+allballs=colb + coli + coln + colg + colo + freespace
 
-raw=str()
+rawinput=str()
+# index of all names. balls (0-74) and freespace (75). called are set to green.
 idxToBall=dict()
+# list of balls on a card. uses index to get names.
 cardcolb=list()
 cardcoli=list()
 cardcoln=list()
 cardcolg=list()
 cardcolo=list()
+winners=list()
+
 
 
 ########## functions ##########
+
 
 
 def flowerBox(message):
@@ -66,41 +75,68 @@ def flowerBox(message):
     print("-" * ( 6 + mlen + 6))
     print("-" * 5 + " " + message + " " + "-" * 5)
     print("-" * ( 6 + mlen + 6))
+    logging.info('event %s',message)
+
 
 
 def newcard():
     """ create a new bingo card """
+    # TODO list of card objects
     # shuffle each column to create a new card
     global cardcolb
     global cardcoli
     global cardcoln
     global cardcolg
     global cardcolo
+    global winners
+    # load a list of numbers matching index into the columns
     cardcolb = list(range(15))
     cardcoli = list(range(15,30))
     cardcoln = list(range(31,45))
     cardcolg = list(range(46,60))
     cardcolo = list(range(61,75))
+    # shuffle the columns
     random.shuffle(cardcolb)
     random.shuffle(cardcoli)
     random.shuffle(cardcoln)
     random.shuffle(cardcolg)
     random.shuffle(cardcolo)
     # add a free space to the card
-    cardcoln[2]=76
+    cardcoln[2]=75
+    # keep first 5
+    cardcolb[5:]=[]
+    cardcoli[5:]=[]
+    cardcoln[5:]=[]
+    cardcolg[5:]=[]
+    cardcolo[5:]=[]
+    # log it
+    logging.debug('new card col b %s ',cardcolb)
+    logging.debug('new card col i %s ',cardcoli)
+    logging.debug('new card col n %s ',cardcoln)
+    logging.debug('new card col g %s ',cardcolg)
+    logging.debug('new card col o %s ',cardcolo)
+    # win combos
+    winners=[cardcolb,cardcoli,[cardcoln[0],cardcoln[1],cardcoln[3],cardcoln[4]],cardcolg,cardcolo, \
+             [cardcolb[0],cardcoli[0],cardcoln[0],cardcolg[0],cardcolo[0]], \
+             [cardcolb[1],cardcoli[1],cardcoln[1],cardcolg[1],cardcolo[1]], \
+             [cardcolb[2],cardcoli[2],cardcolg[2],cardcolo[2]], \
+             [cardcolb[3],cardcoli[3],cardcoln[3],cardcolg[3],cardcolo[3]], \
+             [cardcolb[4],cardcoli[4],cardcoln[4],cardcolg[4],cardcolo[4]], \
+             [cardcolb[0],cardcoli[1],cardcolg[3],cardcolo[4]], \
+             [cardcolb[4],cardcoli[3],cardcolg[1],cardcolo[0]], \
+             [cardcolb[0],cardcolb[4],cardcolo[0],cardcolo[4]]]
+    print(winners)
+
 
 
 def playgame():
     """ would you like to play a game """
-    global colb
-    global coli
-    global coln
-    global colg
-    global colo
+    global allballs
+    global winners
     global idxToBall
-    global raw
+    global rawinput
 
-    os.system('cls||clear')
+    # os.system('clear||cls')
     print("")
     print("")
     print("")
@@ -108,63 +144,66 @@ def playgame():
     flowerBox("New Game")
     print("")
 
-    # get a new list of all balls
-    allballs=colb + coli + coln + colg + colo
-    # print(allballs)
-
-    # index the new balls
+    # create a fresh list of balls, pulled balls will be change to green
+    # index the new balls 
     idxToBall = { i : allballs[i] for i in range(0, len(allballs) ) }
-
-    # add a Free space to the end of the list and pull it
-    idxToBall[76]="Free"
-    print(idxToBall[76])
-
-    pullball(76)
-
-    # print("")
-    # print("idxToBall")
     # print(idxToBall)
+    # pull free space
+    pullball(75)
 
     # shuffle a list of numbers
     pullOrder=list(range(75))
     random.shuffle(pullOrder)
-    print("")
-    print(pullOrder)
+    logging.debug('new pull order for balls %s ',pullOrder)
 
     # loop through the list of pulled balls
     for x in range(75):
-        os.system('cls||clear')
         # print(pullOrder[x])
         pullball(pullOrder[x])
         printcalled()
         printcard()
         print("")
         print(str(x+1) + " balls called")
+
+        # check for a bingo
+        # print(pullOrder[:x+1])
+        for j in winners:
+            # print("   " + str(len(j)) + "   " + str(j))
+            # print("    " + str(len(set(j).intersection(pullOrder[:x+1]))) + "   " + str(set(j).intersection(pullOrder[:x+1])))
+            if len(j) == len(set(j).intersection(pullOrder[:x+1])):
+                flowerBox("BINGO!")
+                for k in j:
+                    # print(k)
+                    print(idxToBall[k], end='  ')
+                print("")
+
         print("Press <Enter> to draw again, enter \"b\" for a Bingo or enter \"q\" to quit.")
-        raw=input()
-        print(raw)
-        if str(raw) == "":
+        rawinput=input()
+        if str(rawinput) == "":
+            os.system('clear||cls')
             continue
-        if str(raw) == "b":
+        if str(rawinput) == "b":
             flowerBox("BINGO!")
             break
-        if str(raw) == "q":
+        if str(rawinput) == "q":
             flowerBox("Game Over")
             exit()
-        if str(raw) == "x":
+        if str(rawinput) == "x":
             flowerBox("You Died")
             exit()
+
+    # play again with or without a new card
     print("Press <Enter> to play again, enter \"n\" for a new Bingo card or enter \"q\" to quit.")
-    raw=input()
-    print(raw)
-    if str(raw) == "q":
+    rawinput=input()
+    if str(rawinput) == "q":
         flowerBox("Game Over")
         exit()
-    if str(raw) == "x":
+    if str(rawinput) == "x":
         flowerBox("You Died")
         exit()
-    if str(raw) == "n":
+    if str(rawinput) == "n":
         newcard()
+
 
 
 def printcalled():
@@ -172,12 +211,14 @@ def printcalled():
     global headings
     global space
     print("")
-    # print(headings)
-    # for x in range(15):
-    #     print(colb[x] + space + coli[x] + space + coln[x] + space + colg[x] + space + colo[x])
     print(headings)
     for x in range(15):
-        print(idxToBall[x] + space + idxToBall[x+15] + space + idxToBall[x+30] + space + idxToBall[x+45] + space + idxToBall[x+60])
+        print(idxToBall[x] + space + \
+              idxToBall[x+15] + space + \
+              idxToBall[x+30] + space + \
+              idxToBall[x+45] + space + \
+              idxToBall[x+60])
+
 
 
 def printcard():
@@ -185,11 +226,14 @@ def printcard():
     global headings
     global space
     print("")
-    # for x in range(5):
-    #     print( str(cardcolb[x]) + space + str(cardcoli[x]) + space + str(cardcoln[x]) + space + str(cardcolg[x]) + space + str(cardcolo[x]))
     print(headings)
     for x in range(5):
-        print( idxToBall[cardcolb[x]] + space + idxToBall[cardcoli[x]] + space + idxToBall[cardcoln[x]] + space + idxToBall[cardcolg[x]] + space + idxToBall[cardcolo[x]])
+        print( idxToBall[cardcolb[x]] + space + \
+               idxToBall[cardcoli[x]] + space + \
+               idxToBall[cardcoln[x]] + space + \
+               idxToBall[cardcolg[x]] + space + \
+               idxToBall[cardcolo[x]])
+
 
 
 def pullball(ball):
@@ -201,8 +245,10 @@ def pullball(ball):
     print("")
     print(idxToBall[ball])
 
+
      
 ########## main ##########
+
 
 
 newcard()
